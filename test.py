52d5d1c7843511dd04f7bfce23923dcd94ddf529
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2024-01-17 11:46:24 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,23 @@
-import os
 import time
-from dynamixel_sdk import *                    # Uses Dynamixel SDK library
+from dynamixel_sdk import *                    
 
 # Dynamixel settings
-DXL_ID = 3                                     # Dynamixel ID
-BAUDRATE = 57600                               # Dynamixel default baudrate
-DEVICENAME = '/dev/ttyUSB0'                    # Check your device port
+DXL_ID_1 = 2                                    # Dynamixel ID 2
+DXL_ID_2 = 3                                    # Dynamixel ID 3
+BAUDRATE = 57600                                # Dynamixel default baudrate
+DEVICENAME = '/dev/ttyUSB0'                     # Check your device port
 
 # Protocol version
 PROTOCOL_VERSION = 2.0
 
 # Control table addresses
-ADDR_MX_TORQUE_ENABLE = 64                     # Control table address for torque enable
-ADDR_MX_GOAL_VELOCITY = 104                    # Control table address for goal velocity
+ADDR_MX_TORQUE_ENABLE = 64                     
+ADDR_MX_GOAL_VELOCITY = 104                    
 
 # Default settings
-TORQUE_ENABLE = 1                              # Value to enable the torque
-TORQUE_DISABLE = 0                             # Value to disable the torque
-DXL_MOVING_SPEED = 200                         # Dynamixel will rotate at this speed (value : 0~1023)
+TORQUE_ENABLE = 1                              
+TORQUE_DISABLE = 0                             
+DXL_MOVING_SPEED = 200                         
 
 # Initialize PortHandler and PacketHandler instances
 portHandler = PortHandler(DEVICENAME)
@@ -37,35 +37,38 @@ else:
     print("Failed to set the baudrate")
     quit()
 
-# Enable Dynamixel Torque
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE)
-if dxl_comm_result != COMM_SUCCESS:
-    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-elif dxl_error != 0:
-    print("%s" % packetHandler.getRxPacketError(dxl_error))
-else:
-    print("Torque enabled")
+# Enable Dynamixel Torque for both servos
+for DXL_ID in [DXL_ID_1, DXL_ID_2]:
+    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+    else:
+        print(f"Torque enabled for Dynamixel ID: {DXL_ID}")
 
-# Set Dynamixel goal velocity
-dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_MX_GOAL_VELOCITY, DXL_MOVING_SPEED)
-if dxl_comm_result != COMM_SUCCESS:
-    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-elif dxl_error != 0:
-    print("%s" % packetHandler.getRxPacketError(dxl_error))
-else:
-    print("Goal velocity set")
+# Set Dynamixel goal velocity for both servos
+for DXL_ID in [DXL_ID_1, DXL_ID_2]:
+    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_MX_GOAL_VELOCITY, DXL_MOVING_SPEED)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+    else:
+        print(f"Goal velocity set for Dynamixel ID: {DXL_ID}")
 
 # Wait for a bit
 time.sleep(2)
 
-# Disable Dynamixel Torque
-dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE)
-if dxl_comm_result != COMM_SUCCESS:
-    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-elif dxl_error != 0:
-    print("%s" % packetHandler.getRxPacketError(dxl_error))
-else:
-    print("Torque disabled")
+# Disable Dynamixel Torque for both servos
+for DXL_ID in [DXL_ID_1, DXL_ID_2]:
+    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+    else:
+        print(f"Torque disabled for Dynamixel ID: {DXL_ID}")
 
 # Close port
 portHandler.closePort()
