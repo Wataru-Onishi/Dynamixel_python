404c834e2ea172dda31c8ce78d163aba35d9b055
Wataru Onishi <62089701+Wataru-Onishi@users.noreply.github.com>
2024-01-17 11:35:25 +0900

    Update dynamixel_python.py

--- dynamixel_python.py
+++ dynamixel_python.py
@@ -5,6 +5,8 @@ from dynamixel_sdk import *  # Dynamixel SDKのインポート
 DXL1_ID = 2  # モーターID2
 DXL2_ID = 3  # モーターID3
 ADDR_MX_MOVING_SPEED = 104  # MXシリーズの場合の速度制御アドレス
+ADDR_XL_TORQUE_ENABLE = 64  # XL430のトルク有効化アドレス
+TORQUE_ENABLE = 1  # トルクを有効化する値
 BAUDRATE = 57600  # ボーレート
 DEVICENAME = '/dev/ttyUSB0'  # ポート名（Windowsでは'COM1', Linuxでは'/dev/ttyUSB0'など）
 
@@ -25,6 +27,13 @@ if portHandler.setBaudRate(BAUDRATE):
 else:
     print("Failed to change the baudrate")
     quit()
+
+
+
+# トルクを有効化
+packetHandler.write1ByteTxRx(portHandler, DXL1_ID, ADDR_XL_TORQUE_ENABLE, TORQUE_ENABLE)
+packetHandler.write1ByteTxRx(portHandler, DXL2_ID, ADDR_XL_TORQUE_ENABLE, TORQUE_ENABLE)
+
 
 # モーターを動かす（ここで速度や回転方向を設定）
 DXL_MOVING_SPEED = 100  # 例としての速度値
